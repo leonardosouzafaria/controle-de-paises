e38b35b40e192ea50a294c2f80879db24240dae0
Renan Viana <renanvianalc@gmail.com>
2018-09-29 21:18:31 -0300

    Criação de uma classe que contém um método main para criar a chave simetrica usada para cifrar a senha do usuário

---
+++ src/utils/GerarChave.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.swing.JOptionPane;
+
+public class GerarChave 
+{
+	public static void main(String[] args) throws NoSuchAlgorithmException,
+		InvalidAlgorithmParameterException, CertificateException, KeyStoreException, IOException
+	{
+		CryptoAES aes = new CryptoAES();
+		aes.geraChave(new File("C:\\Users\\Renan Viana\\Desktop\\chave.simetrica"));
+	}
+}
